94b3a31733d83538b7ae4b0f9bcf937cbe08417d
Benyberg11 <115043937+Benyberg11@users.noreply.github.com>
2024-06-09 13:06:53 +0200

    Revert "More reverting maybe"
    This reverts commit c4284bd48e1ee94160a3ab82ec2b7a4c8a8740d6.

--- Medical_manager_prototype/clinic/__init__.py
+++ Medical_manager_prototype/clinic/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
-import psycopg2
 import os
 
 app = Flask(__name__)
