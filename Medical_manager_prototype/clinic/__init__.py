6b1800a179c885ec0c9787b358ad87bc46ad5ba9
Benyberg11 <115043937+Benyberg11@users.noreply.github.com>
2024-06-09 12:49:14 +0200

    Maybe revert

--- Medical_manager_prototype/clinic/__init__.py
+++ Medical_manager_prototype/clinic/__init__.py
@@ -2,11 +2,11 @@ from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
-import psycopg2
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'fc089b9218301ad987914c53481bff04'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123@localhost/ProjectDB'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql://postgres:123@localhost/ProjectDB')
 
 # Initialize SQLAlchemy
 db = SQLAlchemy(app)
@@ -19,9 +19,12 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
 
-# Database connection details
-db_conn_string = "dbname='ProjectDB' user='postgres' host='127.0.0.1' password='123'"
-conn = psycopg2.connect(db_conn_string)
+# Initialize DB connection for direct psycopg2 use (if needed)
+if 'DATABASE_URL' in os.environ:
+    conn = psycopg2.connect(os.environ['DATABASE_URL'], sslmode='require')
+else:
+    db_conn_string = "dbname='ProjectDB' user='postgres' host='127.0.0.1' password='123'"
+    conn = psycopg2.connect(db_conn_string)
 
 roles = ["none", "doctor", "patient"]
 print(roles)
@@ -35,6 +38,5 @@ app.register_blueprint(Login)
 app.register_blueprint(Patient)
 app.register_blueprint(Doctor)
 
-
 # Ensure user loader callback is defined
-from clinic.models import load_user  
+from clinic.models import load_user
